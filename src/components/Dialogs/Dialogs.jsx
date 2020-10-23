e5df12e2c20d8d6385604612be54b09904200efd
Salauat <kassymkhanov@gmail.com>
2020-10-23 13:15:58 +0600

    25 map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,7 +19,7 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Dimych'},
         {id: 2, name: 'Andrey'},
         {id: 3, name: 'Sveta'},
@@ -28,7 +28,7 @@ const Dialogs = (props) => {
         {id: 6, name: 'Valera'}
     ]
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How is your it-kamasutra?'},
         {id: 3, message: 'Yo'},
@@ -36,18 +36,28 @@ const Dialogs = (props) => {
         {id: 5, message: 'Yo'}
     ]
 
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
+    let messagesElements = messages.map(m => <Message message={m.message} /> );
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+
+                {
+                    dialogsElements
+                }
+
+                {/*<DialogItem name={dialogs[0].name} id={dialogs[0].id} />
+                <DialogItem name={dialogs[1].name} id={dialogs[1].id} />*/}
 
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message} />
+                {
+                    messagesElements
+                }
 
-
+                {/*<Message message={messages[0].message} />
+                <Message message={messages[1].message} />*/}
 
                 {/*<div className={s.message}>Hi</div>     здесь s.message мой вариант
                 <div className={s.message}>How is your it-kamasutra?</div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,11 +4,13 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
 
-    let postData = [
+    let posts = [
         {id: 1, message: 'This is first message', likesCount: 11},
         {id: 2, message: 'This is second one', likesCount: 12},
         {id: 3, message: 'And one more message', likesCount: 16}
     ]
+
+    let postsElements = posts.map( p => <Post message={p.message} likesCount={p.likesCount} />);
 
     return (
         <div className={s.postsBlock}>
@@ -23,9 +25,9 @@ const MyPosts = () => {
 
             </div>
             <div className={s.posts}>
-                <Post message={postData[0].message} likesCount={postData[0].likesCount} />
-                <Post message={postData[1].message} likesCount={postData[1].likesCount} />
-                <Post message={postData[2].message} likesCount={postData[2].likesCount} />
+                {
+                    postsElements
+                }
 
 
             </div>
